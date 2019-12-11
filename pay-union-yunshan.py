7a5978f78c3b8deb8c4e8ef7fdee8100214cc082
elon <ailovephil@gmail.com>
2019-12-11 08:24:37 +0800

    pay2

--- pay-union-yunshan.py
+++ pay-union-yunshan.py
@@ -13,30 +13,36 @@ from airtest.core.api import *
 from airtest.core.android.adb import *
 from poco.drivers.android.uiautomation import AndroidUiautomationPoco
 
-did=''
-aid=''
-authkey=''
 args=sys.argv[4]
 connect_device(args)
 serial=args.replace('Android:///','')
+
 # 线上环境
+# devdb="/Users/elon/code/pandapayMobile/dev.db"
+# qr="/Users/elon/code/pandapayMobile/"+serial+".png"
+# log="/Users/elon/code/pandapayMobile/log.log"
+# serverip='47.90.36.135'
+
 # ubuntu
 # devdb="/home/andy/pandapayMobile/dev.db"
 # qr="/home/andy/pandapayMobile/"+serial+".png"
 # log="/home/andy/pandapayMobile/log.log"
-# mac
+# serverip='192.168.254.101'
+
+# mac环境
 devdb="/Users/elon/code/pandapayMobile/dev.db"
 qr="/Users/elon/code/pandapayMobile/"+serial+".png"
 log="/Users/elon/code/pandapayMobile/log.log"
+serverip='127.0.0.1'
+
 newadb=ADB(serialno=serial)
 poco = AndroidUiautomationPoco(use_airtest_input=True,screenshot_each_action=False)
 
 auto_setup(__file__)
-start_app('com.unionpay','activity.UPActivityMain')
 
 def opsql(sql,db=1,up=False):
 	if db==1:
-		db = pymysql.connect("47.90.36.135","root","pwd123!@#","pandapay")
+		db = pymysql.connect(serverip,"root","pwd123!@#","pandapay")
 	else:
 		db=sqlite3.connect(devdb)
 	cursor = db.cursor()	 
@@ -63,7 +69,6 @@ def init():
 	if newadb.is_screenon()==False:
 		keyevent('26')
 	if newadb.is_locked()==True:
-		# print(newadb.is_locked())
 		newadb.unlock()
 	start_app('com.unionpay')
 	start_app('com.unionpay','activity.UPActivityMain')
@@ -71,7 +76,7 @@ def init():
 
 def getorder():
 	try:
-		r=requests.get('http://47.90.36.135/api/msg/order?did='+serial)
+		r=requests.get('http://'+serverip+'/api/msg/order?did='+serial)
 		if (r.status_code == 200) and (r.text !=0):
 			order=r.text
 			data=eval(order)
@@ -101,7 +106,7 @@ def getbal():
 	msgid=str(getmsgid())
 	auth=getauthkey(msgid)
 	data='{"name":"Yunshan","type":"bal","msgid":"'+msgid+'","id":"'+did+'","auth":"'+auth+'","data":'+info
-	cmd="curl -d '"+data+"' -s http://47.90.36.135/api/msg/bal"
+	cmd="curl -d '"+data+"' -s http://"+serverip+"/api/msg/bal"
 	os.system(cmd)
 
 def	createOrder(order):
@@ -168,58 +173,66 @@ def openMerchant():
 	# 防止00点分页的特殊处理。服务端同样处理
 	time_int=int(time.time())
 	t=time.strftime('%H%M%S', time.localtime(time_int))
-	if (t>'000030'):
+	if (t>'000010'):
 		poco(text="今天").click()
+	sleep(2)
 	
 # 付款凭证号：31191125862881008231 账户卡号：6217****6674 金额：2.00元 2019/11/26 00:00:47 
 def checkMerchatOrderInfo():
 	time_int=int(time.time())
 	t=time.strftime('%H%M%S', time.localtime(time_int))
-	if (t>'000030'):
-		field='order1'
+	if (t<'000011'):
+		maxno=int(10)
 	else:
-		field='order3'
-	
-	sql="SELECT `%s` FROM `dev` WHERE `serial`='%s';" % (field,serial)
-	ordernum=opsql(sql,2)
-	if ordernum is None:
-		insertsql="INSERT INTO `dev` ('serial')  VALUES ('%s')" % (serial)
-		opsql(insertsql,2,True)
-		num=0
-	elif len(ordernum)>0:
-		num=int(ordernum[0])
-	else:
-		num=0
-	print('已收 [ %s ] 单' % (num))
-	curordernum=poco("com.unionpay:id/view_content_container").child("android.webkit.WebView").offspring("app").child("android.view.View")[1].child("android.view.View")[4].get_text()
-	if int(curordernum)>num:
-		print('新订单 [ %s ] 条' % int(int(curordernum)-num))
+		maxno=int(1000)
+
+	for i in range(5,maxno):
 		info=''
-		orderinfo=poco("com.unionpay:id/view_content_container").child("android.webkit.WebView").offspring("app").child("android.view.View")[1].child("android.view.View")[5].get_text()
+		orderinfo=poco("com.unionpay:id/view_content_container").child("android.webkit.WebView").offspring("app").child("android.view.View")[1].child("android.view.View")[i].get_text()
+		if orderinfo=="暂无更多数据~~":
+			break
 		info=orderinfo.replace(' ','').replace('付款凭证号：','{"payid":"').replace('账户卡号：','","payorAccount":"').replace('金额：','","amount":"').replace('元','","paytime":"')
-		info+='","aid":"'+aid+'"}}'
-		# orderdata=eval(info)
-		# print(orderdata['payid'])
-		msgid=str(getmsgid())
-		auth=getauthkey(msgid)
-		data=str('{"id":"'+did+'", "auth": "'+auth+'", "name": "Yunshan", "type": "notice", "msgid": "'+msgid+'", "sub_time": "'+str(time_int)+'","data":'+info)
-		cmd="curl -d '"+data+"' -s http://47.90.36.135/api/msg/index";
-		os.system(cmd)
-		upsql="UPDATE `dev` SET `%s`='%s' WHERE `serial`= '%s';" % (field,curordernum,serial)
-		opsql(upsql,2,True)
+		info+='","aid":"'+aid+'"}'	
+		orderdata=eval(info)
+		sql="SELECT `state` FROM `order` WHERE `oid`='%s';" % (orderdata['payid'])
+		orderstate=opsql(sql,2)
+		if (orderstate is None) or (orderstate !=1):
+			time_int=str(int(time.time()))
+			msgid=str(getmsgid())
+			auth=getauthkey(msgid)
+			data=str('{"id":"'+did+'", "auth": "'+auth+'", "name": "Yunshan", "type": "notice", "msgid": "'+msgid+'", "sub_time": "'+time_int+'","data":'+info+'}')
+			r=requests.post('http://'+serverip+'/api/msg/index',data=data)
+			if (r.status_code == 200) and (r.text ==1):
+				ostate=1
+			else:
+				ostate=0
+
+			insertsql="INSERT INTO `order` ('oid','state')  VALUES ('%s','%s')" % (orderdata['payid'],ostate)
+			opsql(insertsql,2,True)
+
 
 def getmsgid():
 	time_int=int(time.time())
 	msgid=time_int-1500000000
 	return msgid
 
-# sleep(1000)
 # 初始化数据变量
-cfsql="SELECT `id`,`aid`,`authkey` FROM `device` WHERE `serial`='%s';" % (serial)
-cfinfo=opsql(cfsql)
-did=cfinfo[0]
-aid=cfinfo[1]
-authkey=cfinfo[2]
+def devready():
+	createsql='CREATE TABLE IF NOT EXISTS "order"("oid" Text,"state" Boolean DEFAULT 0 );'
+	newtable=opsql(createsql,2)
+	indexsql='CREATE INDEX IF NOT EXISTS "index_oid" ON "order"("oid");'
+	newtable=opsql(indexsql,2)
+	cfgsql="SELECT `id`,`aid`,`authkey` FROM `device` WHERE `serial`='%s';" % (serial)
+	cfginfo=opsql(cfgsql)
+	global did,aid,authkey
+	did=str(cfginfo[0])
+	aid=str(cfginfo[1])
+	authkey=str(cfginfo[2])
+
+
+# sleep(1000)
+devready()
+start_app('com.unionpay','activity.UPActivityMain')
 while 1==1:
 	now_time=int(time.time()) 
 	t=time.strftime('%H%M', time.localtime(now_time))
@@ -245,5 +258,8 @@ while 1==1:
 			if (getpaystate(order)==1):
 				# 收到钱就查余额
 				getbal()
-		#3 没有订单就休息15秒
+		else:
+			#3 没有订单就休息15秒
+			sleep(5)
 	sleep(10)
+
